b6242711f01b9031e09d677a24484207c036cbc9
wangjia31 <wangjia31@baidu.com>
2021-04-12 13:42:30 +0800

    feat: 目录规范

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+/**
+ * @file eslint 配置文件
+ * ESLint 中文文档：http://eslint.cn/docs/rules/eslint-plugin-vue: https://eslint.vuejs.org/
+ * 百度 EFE 团队使用的 eslint 配置：https://github.com/ecomfe/eslint-config/blob/master/index.js
+ * JavaScript 编码规范(百度): http://gitlab.baidu.com/fe/spec/blob/master/javascript.md#2-1
+ * Vue 组件代码规范(百度): http://gitlab.baidu.com/fe/spec/blob/master/vue.md#2-1
+ * iCode校验规则说明：http://bugbye.baidu.com/rules
+ */
+ module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ['plugin:vue/recommended', '@vue/prettier', '@vue/typescript', 'plugin:prettier/recommended'],
+    rules: {
+        'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+        // 要求使用骆驼拼写法 https://cn.eslint.org/docs/rules/camelcase
+        camelcase: ['error'],
+        'no-console': [
+            process.env.NODE_ENV === 'development' ? 'warn' : 'error',
+            {allow: ['error', 'group', 'groupEnd']}
+        ],
+        'no-debugger': process.env.NODE_ENV === 'development' ? 'warn' : 'error'
+    },
+    parserOptions: {
+        parser: '@typescript-eslint/parser'
+    },
+    overrides: [
+        {
+            files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+            env: {
+                jest: true
+            }
+        }
+    ]
+};
--- vue.config.js
+++ vue.config.js
@@ -65,10 +65,10 @@ function addStyleResource(rule) {
         .loader('style-resources-loader')
         .options({
             patterns: [
-                path.resolve(__dirname, './src/assets/css/element.less'),
-                path.resolve(__dirname, './src/assets/css/baidu-map.less'),
-                path.resolve(__dirname, './src/assets/css/mixins/common-mixins.less'),
-                path.resolve(__dirname, './src/assets/css/mixins/operation-mixins.less')
+                path.resolve(__dirname, './src/styles/element.less'),
+                path.resolve(__dirname, './src/styles/baidu-map.less'),
+                path.resolve(__dirname, './src/styles/mixins/common-mixins.less'),
+                path.resolve(__dirname, './src/styles/mixins/operation-mixins.less')
             ]
         });
 }
